b175ff48752dc5a29bc6b496857b8e98bd047ee4
Akashi <akashi_sai@163.com>
2022-07-31 08:21:29 +0800

    :wrench: chore: update deps

--- packages/components/index.ts
+++ packages/components/index.ts
@@ -3,4 +3,6 @@
 // const ret = testFun(1, 2)
 // console.log('ret', ret)
 
+import 'uno.css'
+
 export * from './button'
--- packages/components/vite.config.ts
+++ packages/components/vite.config.ts
@@ -1,5 +1,8 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import unocss from 'unocss/vite'
+import dts from 'vite-plugin-dts'
+import libCss from 'vite-plugin-libcss'
 
 export default defineConfig({
   build: {
@@ -10,5 +13,12 @@ export default defineConfig({
       fileName: 'use-ui'
     },
   },
-  plugins: [vue()]
+  plugins: [
+    vue(),
+    unocss(),
+    dts({
+      outputDir: 'dist/types'
+    }),
+    libCss(),
+  ]
 })
