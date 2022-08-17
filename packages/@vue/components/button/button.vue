d2ce3dc3ff801eb9013e6e1e0514ad83239f5343
akashi <akashi_sai@163.com>
2022-08-17 17:31:03 +0800

    :tada: feat: use ui vue

---
+++ packages/@vue/components/button/button.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import type { ButtonProps } from './types'
+import { buttonProps } from './types'
+
+const props: ButtonProps = defineProps(buttonProps)
+
+const emit = defineEmits(['click', 'change'])
+
+const onClick = (event: Event) => emit('click', event)
+</script>
+
+<template>
+  <button color-rose-400 color-red-400 @click="onClick($event)">Use Button</button>
+</template>
---
+++ packages/@vue/components/index.ts
@@ -0,0 +1,8 @@
+// import { testFun } from '@use-ui/utils'
+
+// const ret = testFun(1, 2)
+// console.log('ret', ret)
+
+import 'uno.css'
+
+export * from './button'
