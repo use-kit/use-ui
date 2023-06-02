51b2aa217a4c89236eb018fea346e3a72a09658a
Akashi <akashi_sai@163.com>
2023-06-02 08:20:04 +0800

    :sparkles: feat: popover component

---
+++ playgrounds/react/src/pages/popover/index.tsx
@@ -0,0 +1,11 @@
+import { UPopover } from '@use-ui/react'
+
+function Popover() {
+  return (
+    <>
+      <UPopover />
+    </>
+  )
+}
+
+export default Popover
---
+++ playgrounds/vue/src/pages/popover/index.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import { UPopover } from '@use-ui/vue'
+</script>
+
+<template>
+  <div>
+    <UPopover />
+  </div>
+</template>
